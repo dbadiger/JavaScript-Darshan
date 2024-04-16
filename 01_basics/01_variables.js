55757001bd0e2ed5872210076d4770d55c23253c
Darshan Badiger <140099027+dbadiger@users.noreply.github.com>
2024-04-16 04:11:51 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const Id = 123
+let email ="Darshan@google.com"
+var name = "Darshan"
+city = "Hubli"
+let state;
+console.table([Id, email, name, city, state]);
+
+/*
+const - it is used to assign constant values. like unique IDs,these cannot be changed.
+
+var - prefer not to use var. because of issues in block scope and functional scope.
+
+undefined - variable is declared but value is not assigned, then that variabel is undefined.
+
+
+*/
