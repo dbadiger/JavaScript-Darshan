840d123f2c385d8b5e376b70b979b86e3d210011
Darshan Badiger <140099027+dbadiger@users.noreply.github.com>
2024-05-28 04:03:06 +0000

    Promises

--- 09_Advance/promises.js
+++ 09_Advance/promises.js
@@ -110,3 +110,17 @@ fetch('https://jsonplaceholder.typicode.com/users')
 .finally(()=>{
     console.log("Finally Block Excuted")
 })
+
+/*The global 'fetch()' method starts the process of fetching a resource from the network, 
+returning a promise which is fulfilled once the response is available.
+The promise resolves to the 'response' object representing the response to your request. 
+
+in fetch(), if you get HTTP error 404 error, where you will get it? in resolve or reject? 
+Ans: It will be always a response. it is not an error in fetch().
+you will get error in fetch() only when request itself is not happening.
+
+A fetch() promise only rejects when the request fails,
+for example, because of a badly-formed request URL or a network error.
+A fetch() promise does not reject if the server responds with HTTP status codes 
+that indicate errors (404, 504, etc.). Instead, a then() handler must check the Response.ok and/or Response.status properties.
+*/
