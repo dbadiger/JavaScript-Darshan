c380ca1a49cd5aa4b007120f2b5544582019f887
Darshan Badiger <140099027+dbadiger@users.noreply.github.com>
2024-05-29 08:08:34 +0000

    Prototype

---
+++ 10_OOPS/prototype_1.js
@@ -0,0 +1,18 @@
+// let myName = "darshan      "
+// let myChar = "db_vlogs      "
+// console.log(myName.length)
+// console.log(myName.trim().length)
+// console.log(myChar.trim().length)
+// console.log(myChartruelength) //Write method which gives exct lenght of string
+
+
+let myHeros = ["thor", "spiderman"]
+let heroPower = {
+    thor:'hammer',
+    spiderman:'sling',
+
+    getspiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`)
+    }
+}
+
