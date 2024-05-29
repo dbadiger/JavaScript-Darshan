461ba656cc56790a863fd1fb54f8eb02383fb23a
Darshan Badiger <140099027+dbadiger@users.noreply.github.com>
2024-05-29 09:21:39 +0000

    commit

--- 10_OOPS/prototype_1.js
+++ 10_OOPS/prototype_1.js
@@ -7,6 +7,7 @@
 
 
 let myHeros = ["thor", "spiderman"]
+
 let heroPower = {
     thor:'hammer',
     spiderman:'sling',
@@ -16,3 +17,48 @@ let heroPower = {
     }
 }
 
+Object.prototype.darshan = function(){
+    console.log(`Darshan is present in all objects`);
+}
+
+Array.prototype.ArrayDarshan = function(){
+    console.log(`Array function access to myHero Array function`);
+}
+
+// heroPower.darshan();
+// myHeros.darshan()
+myHeros.ArrayDarshan()
+// heroPower.ArrayDarshan()        //Error
+
+const User={
+    username:"darshan", email:"darshan@gmail.com"
+}
+
+const teacher = {
+    makeVideo:true
+}
+const TeachAss = {
+    isAvailable: false
+}
+teacher.__proto__ = User;       //protoypal inheritance (old method)
+
+
+const TASupport = {
+    makeAssignment:'JS Assignment',
+    fullTime:true,
+    __proto__: TeachAss
+}
+
+
+//Modern syntax
+Object.setPrototypeOf(TASupport, teacher)
+
+let anotherUser  = "DarshanBadiger    "
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is:${this.trim().length}`);
+}
+
+anotherUser.trueLength();
+"darshan   ".trueLength();
+"greentea".trueLength()
