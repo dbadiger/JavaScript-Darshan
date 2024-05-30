c8cc9615bd500b901d6fd81df65d6fd6784b10e3
Darshan Badiger <140099027+dbadiger@users.noreply.github.com>
2024-05-30 04:05:23 +0000

    Inheritance

--- 10_OOPS/inheritance.js
+++ 10_OOPS/inheritance.js
@@ -22,4 +22,20 @@ class Teacher extends User{
 
 const myTeacher = new Teacher("darshan", "db@kle.com", "123")
 myTeacher.addCourses();
+myTeacher.logMe();      //Inherites from User Object
+
+const myUser = new User("Hello")
+myUser.logMe()
+// myUser.addCourses()      //doesnot have access to addCourse()
+
+
+// console.log(myUser === Teacher);
+// console.log(User === myUser);
+// console.log(myTeacher === Teacher);
+
+
+console.log(myTeacher instanceof Teacher);      //True
+console.log(myUser instanceof Teacher);         //False
+console.log(myUser instanceof User);      //True
+console.log(myTeacher instanceof User);         //true
 
